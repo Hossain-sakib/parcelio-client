4a050b1b3e6e4687134254dcbc4cc0d67403fde0
Hossain-sakib <Hossain.sakib.x@gmail.com>
2025-12-01 02:27:39 +0600

    Completed basic setup with react router

---
+++ src/pages/shared/ParcelioLogo/ParcelioLogo.jsx
@@ -0,0 +1,12 @@
+import logo from "../../../assets/logo.png";
+
+const ParcelioLogo = () => {
+  return (
+    <div className="flex text-4xl">
+      <img src={logo} alt="Parcelio Logo" />
+      <p>Parcelio</p>
+    </div>
+  );
+};
+
+export default ParcelioLogo;
